7dfe74c63324c05694052fe5beb6e8afb70e6990
09Uno <bruno.a.oliveira.1209@gmail.com>
2023-04-03 18:27:49 -0300

    Project Start

---
+++ src/main/java/tcc/schoolschedulemanager/demo/controllers/TeacherController.java
@@ -0,0 +1,5 @@
+package tcc.schoolschedulemanager.demo.controllers;
+
+public class TeacherController {
+    
+}
---
+++ src/main/java/tcc/schoolschedulemanager/demo/models/TeacherModel.java
@@ -0,0 +1,5 @@
+package tcc.schoolschedulemanager.demo.models;
+
+public class TeacherModel {
+    
+}
